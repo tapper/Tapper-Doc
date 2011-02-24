aa51fe3b6714bc1c55c7695dc1425ea988b1d118
Steffen Schwigon <steffen.schwigon@amd.com>
2011-02-24 15:05:36 +0100

    Initial container project for documentation

---
+++ lib/Tapper/Doc.pm
@@ -0,0 +1,65 @@
+package Tapper::Doc;
+
+use warnings;
+use strict;
+
+our $VERSION = '0.01';
+
+1; # End of Tapper::Doc
+
+=head1 NAME
+
+Tapper::Doc - Central documentation hub
+
+=head1 ABOUT
+
+This module is a documentation hub for all kinds of documentation
+about Tapper.
+
+=head1 AUTHOR
+
+AMD OSRC Tapper Team, C<< <tapper at amd64.org> >>
+
+=head1 BUGS
+
+Please report any bugs or feature requests to C<bug-tapper-doc at
+rt.cpan.org>, or through the web interface at
+L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=Tapper-Doc>.  I will
+be notified, and then you'll automatically be notified of progress on
+your bug as I make changes.
+
+=head1 SUPPORT
+
+You can find documentation for this module with the perldoc command.
+
+    perldoc Tapper::Doc
+
+
+You can also look for information at:
+
+=over 4
+
+=item * RT: CPAN's request tracker
+
+L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=Tapper-Doc>
+
+=item * AnnoCPAN: Annotated CPAN documentation
+
+L<http://annocpan.org/dist/Tapper-Doc>
+
+=item * CPAN Ratings
+
+L<http://cpanratings.perl.org/d/Tapper-Doc>
+
+=item * Search CPAN
+
+L<http://search.cpan.org/dist/Tapper-Doc>
+
+=back
+
+=head1 COPYRIGHT & LICENSE
+
+Copyright 2011 AMD OSRC Tapper Team, all rights reserved.
+
+This program is released under the following license: freebsd
+
