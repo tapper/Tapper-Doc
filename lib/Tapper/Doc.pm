b44c3fdd36e42556a3cc7417f31117db9ae9a90e
Steffen Schwigon <steffen.schwigon@amd.com>
2012-05-16 21:16:29 +0200

    POD rework for Dist::Zilla

--- lib/Tapper/Doc.pm
+++ lib/Tapper/Doc.pm
@@ -6,3 +6,40 @@ use strict;
 
 1; # End of Tapper::Doc
 
+=head1 ABOUT
+
+This module is a documentation hub for all kinds of documentation
+about C<Tapper> - a B<Modular Test Infrastructure, inclusive Operating
+System and Virtualization support>.
+
+=head1 AVAILABLE DOCUMENTATION
+
+=over 4
+
+=item * L<Tapper::Doc::Start::StepByStep|Tapper::Doc::Start::StepByStep>
+
+Installation guide to setup a complete infrastructure with Web
+frontend, reports receiver, client libs and automation layer.
+
+=item * L<Tapper::Doc::FactSheet|Tapper::Doc::FactSheet>
+
+Very high level feature overview.
+
+=back
+
+=head1 GENERAL TAPPER SUPPORT
+
+=head2 IRC
+
+Via IRC you can contact us in channel
+
+ #tapper
+
+on
+
+  irc.freenode.net
+
+
+=head2 Mailing List
+
+There is a mailing list L<tapper@amd64.org|http://www.amd64.org/mailman/listinfo/tapper/>.
